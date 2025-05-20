3fa75d1c159b48b0d991bf1e2caf667aab7bda97
Ariano2700 <kawuai123loco123@gmail.com>
2025-05-20 01:13:58 -0500

    feat: change name page

--- src/components/features/FeaturesSection.tsx
+++ src/components/features/FeaturesSection.tsx
@@ -23,7 +23,7 @@ export function FeaturesSection() {
           </p>
         </motion.div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-3 gap-8">
           {features.map((feature, index) => {
             const Icon = feature.icon; // 👈 renderizamos el componente aquí
             return (
--- src/const/features.ts
+++ src/const/features.ts
@@ -1,5 +1,5 @@
 // ✅ Esto sí es válido en features.ts
-import { FiCoffee, FiUsers, FiCalendar } from "react-icons/fi";
+import { FiCoffee, FiShoppingCart } from "react-icons/fi";
 import { LuChartColumn } from "react-icons/lu";
 import { IconType } from "react-icons";
 
@@ -13,21 +13,29 @@ export const features: Feature[] = [
   {
     icon: FiCoffee,
     title: "Gestión de Inventario",
-    description: "Control preciso de ingredientes y productos con alertas de stock bajo.",
+    description:
+      "Control preciso de productos",
   },
   {
-    icon: FiUsers,
-    title: "Gestión de Personal",
-    description: "Administra turnos, roles y permisos para todo el personal de cafetería.",
-  },
-  {
-    icon: FiCalendar,
-    title: "Planificación de Menús",
-    description: "Crea y programa menús semanales o mensuales con facilidad.",
-  },
+  icon: FiShoppingCart,
+  title: "Gestión de Pedidos",
+  description:
+    "Permite a los usuarios realizar pedidos fácilmente, personalizarlos y hacer seguimiento en tiempo real.",
+},
+  // {
+  //   icon: FiUsers,
+  //   title: "Gestión de Personal",
+  //   description: "Administra turnos, roles y permisos para todo el personal de cafetería.",
+  // },
+  // {
+  //   icon: FiCalendar,
+  //   title: "Planificación de Menús",
+  //   description: "Crea y programa menús semanales o mensuales con facilidad.",
+  // },
   {
     icon: LuChartColumn,
     title: "Reportes y Análisis",
-    description: "Estadísticas detalladas sobre ventas, consumo y preferencias.",
+    description:
+      "Estadísticas detalladas sobre ventas, consumo y preferencias.",
   },
 ];
